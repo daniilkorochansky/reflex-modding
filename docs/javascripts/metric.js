c6a59a380ffa5b9f58b251d2ab833fb432639065
Daniil Korochansky <daniilkorochansky@gmail.com>
2026-09-06 23:26:33 +1000

    fix: yandex metric

--- docs/javascripts/metric.js
+++ docs/javascripts/metric.js
@@ -1,12 +1,14 @@
-(function(m,e,t,r,i,k,a) {m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();
-for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+document.addEventListener("DOMContentLoaded", function () {
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t),k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://yandex.ru", "ym");
 
-ym(112328110, "init", {
-     clickmap:true,
-     trackLinks:true,
-     accurateTrackBounce:true,
-     webvisor:false
+    ym(112328110, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true,
+         webvisor:false
+    });
 });
